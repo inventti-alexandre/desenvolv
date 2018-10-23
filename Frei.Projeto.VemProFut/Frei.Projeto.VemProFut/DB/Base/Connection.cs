a45ac0fe1c26d4f3c90d8566ac1643ae6556432f
Pedro Henrique <PHM1080@outlook.com>
2018-10-23 16:32:51 -0300

    Projeto base criado

---
+++ Frei.Projeto.VemProFut/Frei.Projeto.VemProFut/DB/Base/Connection.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frei.Projeto.VemProFut.DB.Base
+{
+    class Connection
+    {
+        public MySqlConnection Create()
+        {
+            //string connectionString = "server=localhost;database=NOMEDOBANCO;uid=root;password=1234;sslmode=none";
+            string connectionString = "server=104.214.59.125;database=blackfitdb;uid=nsf;password=nsf@2018;sslmode=none";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
