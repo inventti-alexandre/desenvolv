7268b0823db8751a559e845b2f7da55bb80c6b8a
infoA31 <infoA31@INFO19>
2018-10-24 10:49:51 -0200

    DTO dos times em andamento.

--- Frei.Projeto.VemProFut/Frei.Projeto.VemProFut/DB/Time/TimeDTO.cs
+++ Frei.Projeto.VemProFut/Frei.Projeto.VemProFut/DB/Time/TimeDTO.cs
@@ -8,5 +8,32 @@ namespace Frei.Projeto.VemProFut.DB.Time
 {
     class TimeDTO
     {
+        public int id_clubes { get; set; }
+        public string nm_nome { get; set; }
+        public string nm_nome_mascara { get; set; }
+        public long lg_logo { get; set; }
+        public string ps_pais { get; set; }
+        public string et_estado { get; set; }
+        public string cd_cidade { get; set; }
+        public string br_bairro { get; set; }
+        public string ra_rua { get; set; }
+        public int nr_numero { get; set; }
+        public int cp_cep { get; set; }
+        public string cp_complemento_endereco { get; set; }
+        public string nm_estadio { get; set; }
+        public int cn_cnpj { get; set; }
+        public string so_site_oficial { get; set; }
+        public string em_email { get; set; }
+        public int tf_telefone_fixo { get; set; }
+        public int tl_celular { get; set; }
+        public int ac_ano_cliacao { get; set; }
+        public DateTime dt_data_cadastro { get; set; }
+        public int fd_fundadores { get; set; }
+        public string obs_onservacoes { get; set; }
+       
+       
+
+
+
     }
 }
